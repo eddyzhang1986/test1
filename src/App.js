1067b023db14b5652f1cddaf3e5b9ec9a88f5da4
eddyzhang1986 <eddyzhang1986@msn.cn>
2019-10-24 09:36:01 +0800

    split pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
-import Analysis from "./components/Analysis";
+import React, { Suspense, lazy } from "react";
+
 import { ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
 import "./configs/mockApi";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
+
+//分析页
+const Analysis = lazy(() => import("./components/Analysis"));
+
+const Routes = props => {
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <HashRouter>
+        <Route exact path="/analysis">
+          <Analysis />
+        </Route>
+      </HashRouter>
+    </Suspense>
+  );
+};
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
-      <Analysis />
+      <Routes />
     </ConfigProvider>
   );
 }
